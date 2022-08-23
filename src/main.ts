baa471dacc16eabb7415350799de3f27db63945c
Akio Jinsenji <akio-jinsenji@cloud-creative-studios.com>
2022-08-23 13:19:43 +0900

    Initial commit.

---
+++ src/main.ts
@@ -0,0 +1,53 @@
+import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+import { ArgumentBuilder } from '@akiojin/argument-builder'
+
+async function Login(appID: string, password: string, tenant: string)
+{
+    const builder = new ArgumentBuilder()
+        .Append('login')
+        .Append('-u', appID)
+        .Append('-p', password)
+        .Append('--tenant', tenant)
+    
+    await exec.exec('az', builder.Build())
+}
+
+async function GetPublishProfile(appName: string, resourceGroup: string, subscription: string)
+{
+    const builder = new ArgumentBuilder()
+        .Append('webapp')
+        .Append('deployment')
+        .Append('list-publishing-profiles')
+        .Append('--name', appName)
+        .Append('--resource-group', resourceGroup)
+        .Append('--subscription', subscription)
+        .Append('--xml')
+    
+    let output: string = ''
+    const options: exec.ExecOptions = {
+        listeners: {
+            stdout: (data: Buffer) => {
+                output += data.toString()
+            }
+        }
+    }
+
+    await exec.exec('az', builder.Build(), options)
+
+    return output;
+}
+
+async function Run()
+{
+    try {
+        await Login(core.getInput('app-id'), core.getInput('password'), core.getInput('tenant'))
+
+        const publishProfile = GetPublishProfile(core.getInput('app-name'), core.getInput('resource-group'), core.getInput('subscription'))
+        core.setOutput('publish-profile', publishProfile)
+    } catch (ex: any) {
+        core.setFailed(ex.message)
+    }
+}
+
+Run()
